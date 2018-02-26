a90f1790bd20e2e3caefc433d52ff285ff8fe41c
Lamprecht <lamprechtsebastian@gmx.de>
2018-02-26 13:35:19 +0100

    __init__adapted

--- pointspy/__init__.py
+++ pointspy/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '0.1'
 
-from indexkd import IndexKD
-from extent import Extent
-from georecords import GeoRecords
-from grid import Grid
-from config import Config
+from .indexkd import IndexKD
+from .extent import Extent
+from .georecords import GeoRecords
+from .grid import Grid
+from .config import Config
