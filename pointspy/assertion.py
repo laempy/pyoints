19ee05b136c4f96d472f5a6eda541e0c207f974c
Lamprecht <lamprecht@uni-trier.de>
2018-03-14 13:56:01 +0100

    assertion replaced with value errors

--- pointspy/assertion.py
+++ pointspy/assertion.py
@@ -1,33 +1,41 @@
 import numpy as np
 
 def ensure_coords(coords, by_row=False):
-
-    assert hasattr(coords, '__len__')
+    if not hasattr(coords, '__len__'):
+        raise ValueError("coords has no length")
 
     if not isinstance(coords, np.ndarray):
         coords = np.array(coords)
     if by_row:
         coords = coords.T
 
-    assert len(coords.shape) == 2
-    assert coords.shape[1] > 0
+    if not len(coords.shape) == 2:
+        raise ValueError("malformed shape of coordinates")
+    if not coords.shape[1] > 1:
+        raise ValueError("at least two coordinate dimensions needed")
+
     return coords
 
 
 def ensure_polar(pcoords, by_row=False):
     pcoords = ensure_coords(pcoords, by_row=by_row)
-    assert np.all(pcoords[:,0] >= 0)
+    if not np.all(pcoords[:, 0] >= 0):
+        raise ValueError("malformed polar radii")
     return pcoords
 
 
 def ensure_tmatrix(T):
 
-    assert hasattr(T, '__len__')
+    if not hasattr(T, '__len__'):
+        raise ValueError("transformation matrix has no length")
     if not isinstance(T, np.matrix):
         T = np.matrix(T)
 
-    assert len(T.shape) == 2
-    assert T.shape[0] == T.shape[1]
-    assert T.shape[0] > 2
+    if not len(T.shape) == 2:
+        raise ValueError("malformed shape of transformation matrix")
+    if not T.shape[0] == T.shape[1]:
+        raise ValueError("transformation matrix is not a square matrix")
+    if not T.shape[0] > 2:
+        raise ValueError("at least two coordinate dimensions needed")
 
     return T
