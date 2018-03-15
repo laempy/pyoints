0bdf94bb4ed80df362b87d824becb77915132475
Lamprecht <lamprecht@uni-trier.de>
2018-03-15 15:16:24 +0100

    transformation revised

--- pointspy/transformation.py
+++ pointspy/transformation.py
@@ -1,9 +1,13 @@
+"""Multidimensional transformation matrices and coordinate transformations.
+"""
+from numbers import Number
 import numpy as np
+import itertools as it
 
-from . import distance
-from . import assertion
-
-# TODO documentation
+from . import (
+    distance,
+    assertion,
+)
 
 
 def transform(coords, T, inverse=False):
@@ -12,14 +16,14 @@ def transform(coords, T, inverse=False):
 
     Parameters
     ----------
-    coords : (n,k), array_like
+    coords : array_like(Number, shape=(n,k))
         Represents n data points of `k` dimensions.
-    T : (k+1,k+1), array_like
+    T : array_like(Number, shape=(k+1,k+1))
         Transformation matrix.
 
     Returns
     -------
-    coords : (n,k), array_like
+    coords : np.ndarray(Number, shape=(n,k))
         Transformed coordinates.
 
     """
@@ -38,10 +42,24 @@ def transform(coords, T, inverse=False):
 
 
 def homogenious(coords, value=1):
+    """Create homogenious coordinates by adding an additional dimension.
 
-    assert hasattr(coords, '__len__')
-    if not isinstance(coords, np.ndarray):
-        coords = np.array(coords)
+    Parameters
+    ----------
+    coords : array_like(Number, shape=(n,k))
+        Represents n data points of `k` dimensions.
+    value : optional, Number
+        Values to set.
+
+    Returns
+    -------
+    hcoords : np.ndarray(Number, shape=(n,k+1))
+        Homogenious coordinates.
+
+    """
+    coords = assertion.ensure_coords(coords)
+    if not isinstance(value, Number):
+        raise ValueError("'value' needs to be numeric")
 
     if len(coords.shape) == 1:
         H = np.append(coords, value)
@@ -53,71 +71,237 @@ def homogenious(coords, value=1):
     return H
 
 
-def matrix(t=None, r=None, s=None):
+def matrix(t=None, r=None, s=None, order='trs'):
+    """Creates a transformation matrix based on translation, rotation and scale
+    coefficients.
+
+    Parameters
+    ----------
+    t,r,s : optional, np.ndarray(Number, shape=(k))
+        Transformation coefficients for translation `t`, rotation `r` and
+        scale `s`.
+    order : optional, string
+        Order to compute the matrix multiplications. A `order` of 'trs' means
+        first translate `t`, then rotate `r` and finally scale `s`.
+
+    Returns
+    -------
+    LocalSystem(shape=(k+1,k+1))
+        Transformation matrix according to arguments `t`, `r`, `s` and order
+        `order`.
+
+    See Also
+    --------
+    LocalSystem, t_matrix, r_matrix, s_matrix
+
+    """
     shape = (0, 0)
+    keys = ('t', 'r', 's')
+    orders = [''.join(o) for o in it.permutations(keys)]
 
+    if not isinstance(order, str):
+        raise ValueError("order needs to be of type string")
+    if order not in orders:
+        raise ValueError("order '%s' unknown" % order)
+
+    if t is None and r is None and s is None:
+        raise ValueError("at least one attribute neets to be provided")
+
+    # create the matrices
+    matrices = {}
     if t is not None:
-        tM = t_matrix(t)
-        shape = tM.shape
+        matrices['t'] = t_matrix(t)
+        shape = matrices['t'].shape
     if r is not None:
-        rM = r_matrix(r)
-        shape = rM.shape
+        matrices['r'] = r_matrix(r)
+        shape = matrices['r'].shape
     if s is not None:
-        sM = s_matrix(s)
-        shape = sM.shape
+        matrices['s'] = s_matrix(s)
+        shape = matrices['s'].shape
 
+    # doble check the shape
     assert len(shape) == 2
     assert shape[0] == shape[1]
     assert shape[0] > 1
 
-    if t is None:
-        tM = i_matrix(shape[0] - 1)
-    if r is None:
-        rM = i_matrix(shape[0] - 1)
-    if s is None:
-        sM = i_matrix(shape[0] - 1)
+    # fill missing matrices with identity matrices
+    for key in keys:
+        if key not in matrices.keys():
+            matrices[key] = i_matrix(shape[0] - 1)
 
-    assert tM.shape == shape
-    assert rM.shape == shape
-    assert sM.shape == shape
+    # check dimensions
+    for M in matrices.values():
+        if not M.shape == shape:
+            raise ValueError("matrix dimensions differ")
 
-    return tM * rM * sM
+    # create translation matrix according to order
+    M = i_matrix(shape[0] - 1)
+    for key in list(order):
+        M = M * matrices[key]
+
+    return LocalSystem(M)
 
 
-def i_matrix(d):
-    assert isinstance(d, int) and d > 1
-    return np.matrix(np.identity(d + 1))
+def i_matrix(dim):
+    """Creates a identity transformation matrix.
+
+    Parameters
+    ----------
+    dim : positive int
+        Desired dimension of the transformation matrix. At least a value of
+        two is reqired.
+
+    Returns
+    -------
+    LocalSystem(int, shape=(dim+1,dim+1))
+        Identity transformation matrix.
+
+    See Also
+    --------
+    LocalSystem
+
+    Examples
+    --------
+
+    >>> print i_matrix(3)
+    [[1. 0. 0. 0.]
+     [0. 1. 0. 0.]
+     [0. 0. 1. 0.]
+     [0. 0. 0. 1.]]
+
+    """
+    if not (isinstance(dim, int) and dim >= 2):
+        raise ValueError("'dim' needs to be an integer greater one")
+    I = np.matrix(np.identity(dim + 1))
+    return LocalSystem(I)
 
 
 def t_matrix(t):
-    assert hasattr(t, '__len__') and len(t) > 1
+    """Creates a translation matrix.
+
+    Parameters
+    ----------
+    t : array_like(Number,shape=(k))
+        Translation coefficients for each coordinate dimension. At least two
+        coefficients are required.
+
+    Returns
+    -------
+    LocalSystem(shape=(k+1,k+1))
+        Translation matrix.
+
+    See Also
+    --------
+    LocalSystem
+
+    Examples
+    --------
+
+    >>> print t_matrix([1,2,3])
+    [[1. 0. 0. 1.]
+     [0. 1. 0. 2.]
+     [0. 0. 1. 3.]
+     [0. 0. 0. 1.]]
+
+    """
+    if not (hasattr(t, '__len__') and len(t) >= 2):
+        raise ValueError("'t' needs have a length greater one")
     dim = len(t)
     T = np.identity(dim + 1)
     T[0:dim, dim] = t
-    return np.matrix(T)
+    return LocalSystem(T)
 
 
 def s_matrix(s):
-    assert hasattr(s, '__len__') and len(s) > 1
+    """Creates a scaling matrix.
+
+    Parameters
+    ----------
+    s : array_like(Number,shape=(k))
+        Scaling coefficients for each coordinate dimension. At least two
+        coefficients are required.
+
+    Returns
+    -------
+    LocalSystem(shape=(k+1,k+1))
+        Scaling matrix.
+
+    See Also
+    --------
+    LocalSystem
+
+    Examples
+    --------
+
+    >>> print s_matrix([0.5,1,-3])
+    [[ 0.5  0.   0.   0. ]
+     [ 0.   1.   0.   0. ]
+     [ 0.   0.  -3.   0. ]
+     [ 0.   0.   0.   1. ]]
+
+    """
+    if not (hasattr(s, '__len__') and len(s) >= 2):
+        raise ValueError("'s' needs have a length greater one")
     dim = len(s)
     S = np.identity(dim + 1)
     diag = np.append(s, 1)
     np.fill_diagonal(S, diag)
-    return np.matrix(S)
+    return LocalSystem(S)
 
 
 def r_matrix(a):
+    """Creates a rotation matrix.
 
+    Parameters
+    ----------
+    r : Number or array_like(Number,shape=(k))
+        Rotation coefficients for each coordinate dimension. At least two
+        coefficients are required.
+
+    Returns
+    -------
+    LocalSystem(shape=(k+1,k+1))
+        Rotation matrix.
+
+    See Also
+    --------
+    LocalSystem
+
+    Notes
+    -----
+    Currently only two and tree coordinate dimensions are supported yet.
+
+    Examples
+    --------
+
+    Two dimensional case.
+
+    >>> R = r_matrix(np.pi/4)
+    >>> print np.round(R,3)
+    [[ 0.707 -0.707  0.   ]
+     [ 0.707  0.707  0.   ]
+     [ 0.     0.     1.   ]]
+
+    Three dimensional case.
+
+    >>> R = r_matrix([np.pi/2,0,np.pi/4])
+    >>> print np.round(R,3)
+    [[ 0.707 -0.     0.707  0.   ]
+     [ 0.707  0.    -0.707  0.   ]
+     [ 0.     1.     0.     0.   ]
+     [ 0.     0.     0.     1.   ]]
+
+    """
     if isinstance(a, float):
         R = np.matrix([
             [np.cos(a), -np.sin(a), 0],
             [np.sin(a), np.cos(a), 0],
             [0, 0, 1]
         ])
-    else:
-        assert hasattr(a, '__getitem__')
+    elif hasattr(a, '__getitem__'):
+        a = assertion.ensure_vector(a)
         if len(a) == 2:
-            raise ValueError('Rotation in 2D requires one angle only.')
+            raise ValueError('rotation in 2D requires one angle only')
         elif len(a) == 3:
             Rx = np.matrix([
                 [1, 0, 0, 0],
@@ -142,20 +326,87 @@ def r_matrix(a):
             raise ValueError(
                 '%i-dimensional rotations are not supported yet' %
                 len(a))
-    return R
+    else:
+        raise ValueError("'r' needs be numeric or an iterable of numbers")
+
+    return LocalSystem(R)
 
 
 def add_dim(T):
+    """Adds a dimension to a transformation matrix.
 
-    T = _assert.ensure_tmatrix(T)
+    Parameters
+    ----------
+    T : array_like(Number, shape=(k+1,k+1))
+        Transformation matrix of `k` coordinate dimensions.
 
-    dim = len(T) + 1
-    M = np.eye(dim)
-    M[:-2, :-2] = T
+    Returns
+    -------
+    np.matrix(float, shape=(k+2,k+2))
+        Transformation matrix with an additional dimension.
+
+
+    Examples
+    --------
+
+    Two dimensional case.
+
+    >>> T = matrix(t=[2,3 ], s=[0.5, 2])
+    >>> print np.round(T, 3)
+    [[0.5 0.  2. ]
+     [0.  2.  3. ]
+     [0.  0.  1. ]]
+    >>> T = add_dim(T)
+    >>> print np.round(T, 3)
+    [[0.5 0.  0.  2. ]
+     [0.  2.  0.  3. ]
+     [0.  0.  1.  0. ]
+     [0.  0.  0.  1. ]]
+
+    """
+    T = assertion.ensure_tmatrix(T)
+    M = np.eye(len(T)+1)
+    M[:-2, :-2] = T[:-1, :-1]
+    M[:-2, -1] = T[:-1, -1].T
     return M
 
 
 def decomposition(T):
+    """Determinates most important parameters of a transformation matrix.
+
+    Parameters
+    ----------
+    T : array_like(Number, shape=(k+1,k+1))
+        Transformation matrix of `k` coordinate dimensions.
+
+    Returns
+    -------
+    t,r,s : optional, np.ndarray(Number, shape=(k))
+        Transformation coefficients for translation `t`, rotation `r` and
+        scale `s`.
+    det : float
+        Derterminant `det` indicates a distorsion of `T`, if not `det` == 1.
+
+    Examples
+    --------
+
+    >>> T = matrix(t=[2, 3], r=0.2, s=[0.5, 2])
+    >>> t, r, s, det = decomposition(T)
+    >>> print t
+    [2. 3.]
+    >>> print r
+    0.2
+    >>> print s
+    [0.5 2. ]
+    >>> print det
+    1.0
+
+    See Also
+    --------
+    matrix
+
+    """
+    # TODO references
     # https://math.stackexchange.com/questions/237369/given-this-transformation-matrix-how-do-i-decompose-it-into-translation-rotati
     # https://math.stackexchange.com/questions/13150/extracting-rotation-scale-values-from-2d-transformation-matrix/13165#13165
 
@@ -191,40 +442,249 @@ def decomposition(T):
 
 
 class LocalSystem(np.matrix, object):
+    """Defines a local coordinate system based on a transformation matrix.
+
+    Parameters
+    ----------
+    T : array_like(Number, shape=(k+1, k+1))
+        Transformation matrix of `k` coordinate dimensions.
+
+    Attributes
+    ----------
+    dim : positive int
+        Number of coordinate dimensions.
+    components : np.ndarray(Number, shape=(k+1, k+1))
+        Components of the local coordinate system. Each component represents
+        a the direction vector of the coordinate axis in the global coordinate
+        system.
+    origin :
+        Global origin of the local coordinate system.
+
+    Examples
+    --------
+
+    >>> T = assertion.ensure_tmatrix([(2,-0.5,2),(0.5,1,3),(0,0,1)])
+    >>> print  T
+    [[ 2.  -0.5  2. ]
+     [ 0.5  1.   3. ]
+     [ 0.   0.   1. ]]
+    >>> L = LocalSystem(T)
+    >>> L.dim
+    2
+    >>> print np.round(L.components, 3)
+    [[ 2.  -0.5]
+     [ 0.5  1. ]]
+    >>> print np.round(L.origin, 3)
+    [2. 3.]
+
+    See Also
+    --------
+    matrix
+
+    """
 
     def __new__(cls, T):
-        return np.matrix(T, dtype=float).view(cls)
+        return assertion.ensure_tmatrix(T).view(cls)
 
     @property
     def dim(self):
         return len(self) - 1
 
-    def to_local(self, globalCoords):
-        return transform(globalCoords, self)
-
-    def to_global(self, localCoords):
-        return transform(localCoords, self, inverse=True)
-
-    def distance(self, coords):
-        return distance.norm(self.toLocal(coords)[:, 1:self.dim])
-
-    def PC(self, k):
-        assert k <= self.dim
-        pc = self[k - 1, :self.dim]
-        return np.asarray(pc)[0]
-
-    def explained_variance(self, globalCoords):
-        localCoords = self.to_local(globalCoords)
-        return np.var(localCoords, axis=0)
-
-    def explained_variance_ratio(self, globalCoords):
-        var = self.explained_variance(globalCoords)
-        return var / var.sum()
-
     @property
     def components(self):
-        return self[:self.dim, :self.dim]
+        # TODO here might be a sign error
+        return np.asarray(self[:self.dim, :self.dim])
 
     @property
     def origin(self):
-        return -np.asarray(self[:self.dim, self.dim]).T[0]
+        return np.asarray(self[:self.dim, self.dim]).T[0]
+
+    def to_local(self, gcoords):
+        """Transforms global coordinates into local coordinates.
+
+        Parameters
+        ----------
+        gcoords : array_like(Number, shape=(n,k))
+            Represents n data points of `k` dimensions in the global coordinate
+            system.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(n,k))
+            Local coordinates.
+
+        Examples
+        --------
+
+        >>> T = matrix(t=[2, 3],s=[0.5,10])
+        >>> lcoords = T.to_local([(0,0),(0,1),(1,0),(-1,-1)])
+        >>> print lcoords
+        [[ 2.   3. ]
+         [ 2.  13. ]
+         [ 2.5  3. ]
+         [ 1.5 -7. ]]
+
+        """
+        return transform(gcoords, self)
+
+    def to_global(self, lcoords):
+        """Transforms local coordinates into global coordinates.
+
+        Parameters
+        ----------
+        lcoords : array_like(Number, shape=(n,k))
+            Represents n data points of `k` dimensions in the local coordinate
+            system.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(n,k))
+            Global coordinates.
+
+        Examples
+        --------
+
+        >>> T = matrix(t=[2, 3],s=[0.5,10])
+        >>> lcoords = T.to_global([(2,3),(2,13),(2.5,3),(1.5,-7)])
+        >>> print lcoords
+        [[ 0.  0.]
+         [ 0.  1.]
+         [ 1.  0.]
+         [-1. -1.]]
+
+        """
+        return transform(lcoords, self, inverse=True)
+
+    def distance(self, gcoords):
+        """Transforms global coordinates into local coordinates.
+
+        Parameters
+        ----------
+        gcoords : array_like(Number, shape=(n,k))
+            Represents n data points of `k` dimensions in the global coordinate
+            system.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(n))
+            Distances of the coordinates to the first principal component of
+            the coordinate system. The first principal compnent corresponds to
+            the first
+
+        Examples
+        --------
+
+        >>> T = matrix(t=[2, 3], s=[0.5, 10])
+        >>> dist = T.distance([(0,0),(0,1),(1,0),(-1,-1)])
+        >>> print dist
+        [ 3. 13.  3.  7.]
+
+        """
+        lcoords = self.to_local(gcoords)
+        return distance.norm(lcoords[:, 1:self.dim])
+
+    def PC(self, k):
+        """Get a specific principal component of the local coordinate system.
+
+        Parameters
+        ----------
+        k : positive int
+            `k` th principal component to select.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(self.dim))
+            `k` th principal component.
+
+        Examples
+        --------
+
+        >>> T = matrix(t=[2, 3], s=[0.5, 10])
+        >>> print T
+        [[ 0.5  0.   2. ]
+         [ 0.  10.   3. ]
+         [ 0.   0.   1. ]]
+        >>> print T.PC(1)
+        [ 0. 10.]
+        >>> print T.PC(2)
+        [0.5 0. ]
+
+        """
+        if not (k >= 1 and k <= self.dim):
+            raise ValueError("%'th principal component not available")
+
+        order = np.argsort(distance.snorm(self.components)[::-1])
+        i = np.where(order == (k-1))[0][0]
+        pc = self[i, :self.dim]
+        return np.asarray(pc)[0]
+
+    def explained_variance(self, gcoords):
+        """Get explained variance of global coordinates.
+
+        Parameters
+        ----------
+        gcoords : array_like(Number, shape=(n,k))
+            Represents n data points of `k` dimensions in the global coordinate
+            system.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(self.dim))
+            Total amount of explained variance by a specific coordinate axis.
+
+        Examples
+        --------
+
+        >>> T = s_matrix([1, 2])
+        >>> print T
+        [[1. 0. 0.]
+         [0. 2. 0.]
+         [0. 0. 1.]]
+        >>> e = T.explained_variance([(2,1),(0,0),(1,1),(2,3)])
+        >>> print np.round(e, 3)
+        [0.688 4.75 ]
+
+        See Also
+        --------
+        LocalSystem.explained_variance_ratio
+
+        """
+        lcoords = self.to_local(gcoords)
+        return np.var(lcoords, axis=0)
+
+    def explained_variance_ratio(self, gcoords):
+        """Get relative amount of explained variance of global coordinates.
+
+        Parameters
+        ----------
+        gcoords : array_like(Number, shape=(n,k))
+            Represents n data points of `k` dimensions in the global coordinate
+            system.
+
+        Returns
+        -------
+        np.ndarray(Number, shape=(self.dim))
+            Relative amount of explained variance by a specific coordinate
+            axis.
+
+
+        Examples
+        --------
+
+        >>> T = s_matrix([1, 2])
+        >>> print T
+        [[1. 0. 0.]
+         [0. 2. 0.]
+         [0. 0. 1.]]
+        >>> e = T.explained_variance_ratio([(2,1),(0,0),(1,1),(2,3)])
+        >>> print np.round(e, 3)
+        [0.126 0.874]
+
+        See Also
+        --------
+        LocalSystem.explained_variance
+
+        """
+        var = self.explained_variance(gcoords)
+        return var / var.sum()
+
