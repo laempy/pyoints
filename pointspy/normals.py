7d1b0341f8109843590a795101755f5cedd72e67
Sebastian Lamprecht <lamprecht@uni-trier.de>
2018-07-17 07:22:20 +0200

    experiments with normals

--- pointspy/normals.py
+++ pointspy/normals.py
@@ -9,8 +9,9 @@ from . import (
     Coords,
     distance,
 )
+from .misc import *
 
-def find_normals(coords, radii, indices=None, prefered_normal=None):
+def find_normals(coords, radii, k=5, indices=None, prefered_normal=None):
     """Calculate normals from coordinates based on neighbouring points. 
     
     Parameters
@@ -60,11 +61,22 @@ def find_normals(coords, radii, indices=None, prefered_normal=None):
     
     # generate normals
     normals = np.zeros((len(indices), dim), dtype=float)
-    ball_gen = coords.indexKD().ball_iter(coords[indices, :], radii)
+    #ball_gen = coords.indexKD().ball_iter(coords[indices, :], radii)
+    ball_gen = coords.indexKD().balls_iter(coords[indices, :], radii)
+    #knn_gen = coords.indexKD().knn_iter(
+    #        coords[indices, :], k=k, distance_upper_bound=max_distance)
+    tic()
+    #for i, (dists, nIds) in enumerate(knn_gen):
     for i, nIds in enumerate(ball_gen):
-
+        #nIds = [nIds[i] for i in range(len(nIds)) if dists[i] < max_distance]
+        #coords[nIds, :]
         if len(nIds) >= dim:
             normals[i, :] = PCA(coords[nIds, :]).pc(dim)
+        if i % 10000 == 0:
+            toc()
+            tic()
+            print(i)
+            
                 
     # flip normals if required
     dists = distance.snorm(normals - prefered_normal)
@@ -72,3 +84,71 @@ def find_normals(coords, radii, indices=None, prefered_normal=None):
     
     return normals
             
+
+def approximate_normals(coords, r, prefered_normal=None):
+    """Calculate normals from coordinates based on neighbouring points. 
+    
+    Parameters
+    ----------
+    coords : array_like(Number, shape=(n, k))
+        Represents `n` points of `k` dimensions.
+    radii : positive Number or array_like(Number, shape=(n))
+        Radius or radii to select neighbouring points.
+        
+    Returns
+    -------
+    array_like(Number, shape=(n, k))
+        Desired normals of coordinates `coords`.
+    
+    Examples
+    --------
+    
+    Two dimensional normals.
+    
+    >>> coords = [(0, 0), (1, 1), (2, 3), (3, 3), (4, 2), (5, 1), (5, 0)]
+    >>> normals = find_normals(coords, 1.5)
+    >>> print(np.round(normals, 2))
+    [[-0.71  0.71]
+     [-0.71  0.71]
+     [ 0.    1.  ]
+     [ 0.47  0.88]
+     [ 0.71  0.71]
+     [ 0.88  0.47]
+     [ 1.    0.  ]]
+    
+    """
+    coords = Coords(coords)
+    dim = coords.dim
+    
+    # define prefered normal
+    if prefered_normal is None:
+        prefered_normal = np.zeros(dim)
+        prefered_normal[-1] = 1
+    else:
+        prefered_normal = assertion.ensure_numvector(length=dim)
+    
+    # generate normals
+    normals = np.zeros(coords.shape, dtype=float)
+
+    #knn_gen = coords.indexKD().knn_iter(
+    #        coords[indices, :], k=k, distance_upper_bound=max_distance)
+    tic()
+    #for i, (dists, nIds) in enumerate(knn_gen):
+    for i in range(len(coords)):
+        #nIds = [nIds[i] for i in range(len(nIds)) if dists[i] < max_distance]
+        #coords[nIds, :]
+        if normals[i].sum() == 0:
+            nIds = coords.indexKD().ball(coords[i, :], r)
+            if len(nIds) >= dim:
+                normals[nIds, :] = PCA(coords[nIds, :]).pc(dim)
+        if i % 10000 == 0:
+            toc()
+            tic()
+            print(i)
+            
+                
+    # flip normals if required
+    dists = distance.snorm(normals - prefered_normal)
+    normals[dists > 2] *= -1
+    
+    return normals
