300352965f05d5171f7fd1bc178515fc10487b0a
Lamprecht <lamprechtsebastian@gmx.de>
2018-08-07 12:27:18 +0200

    new grid example

--- examples/grid_example.py
+++ examples/grid_example.py
@@ -1,35 +1,45 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  1 10:52:39 2018
+"""Test to read and write raster files."""
 
-@author: sebastian
-"""
+import os
 
 from pointspy import (
     storage,
     transformation,
+    projection,
 )
 
-infile = '/home/sebastian/Downloads/SP27GTIF.TIF'
-infile = '/home/sebastian/Downloads/cea.tif'
 
-outfile = '/home/sebastian/Schreibtisch/temp/test.tif'
-rasterHandler = storage.RasterReader(infile)
+inpath = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), '..', 'images')
+outpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
 
+
+# load image
+#############
+
+infile = os.path.join(inpath, 'logo.png')
+print("load %s" % infile)
+proj = projection.Proj.from_epsg(32632)
+rasterHandler = storage.RasterReader(infile, proj=proj)
+grid = rasterHandler.load()
+print(grid.shape)
+print(grid.dtype.descr)
+
+# apply transformation
+T = transformation.matrix(
+        t=[332575, 5513229], s=[0.5, -0.5], r=0.1, order='srt')
+grid.transform(T)
+
+
+# save image and load again
+############################
+
+outfile = os.path.join(outpath, 'test.tif')
+print("save %s" % outfile)
+storage.writeRaster(grid, outfile)
+
+print("load %s" % outfile)
+rasterHandler = storage.RasterReader(outfile, proj=projection.Proj())
 grid = rasterHandler.load()
 
-print grid.t
-print rasterHandler.date
-
-storage.writeRaster(grid, outfile)
-
-
-grid.t = grid.t * transformation.s_matrix([2, 2])
-print grid.t
-storage.writeRaster(grid, outfile)
-
-t = transformation.decomposition(grid.t)[0]
-grid.t = grid.t * transformation.matrix(s = [20, 20], r=0.1)
-grid.t = transformation.matrix(t = t, s = [20, -20], r=0.1)
-print grid.t
-storage.writeRaster(grid, outfile)
+print(grid.t.origin)
