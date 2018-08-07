95c7ff2545ac5fb266d6f4b92d93d06abbb8564e
Lamprecht <lamprechtsebastian@gmx.de>
2018-08-07 09:01:46 +0200

    pip installation revised

--- pipInstall.sh
+++ pipInstall.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-venv=$1
+venv="venv"
+requirements="requirements.txt"
+usage="Usage: pipInstall.sh -v venv -r requirements.txt"
+
+while getopts r:v:h option
+do
+    case "${option}" in
+    r) requirements=${OPTARG};;
+    v) venv=${OPTARG};;
+    h)
+        echo $usage
+        exit 0
+    ;;
+    \?)
+        echo $usage
+        exit 0
+    ;;
+    esac
+done
+
 if [ -n "$venv" ]
 then
     source ./${venv}/bin/activate
-    pip install pygdal==$(gdal-config --version).* -r requirements.txt --upgrade
+    pip install pygdal==$(gdal-config --version).* -r $requirements --upgrade
 else
    echo 'Please specify virtualenv'
 fi
