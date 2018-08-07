0fe6fd3283922424d6425979222a9ef2f9dd7f9b
Sebastian Lamprecht <lamprecht@uni-trier.de>
2018-08-07 08:08:29 +0200

    pip --upgrade

--- pipInstall.sh
+++ pipInstall.sh
@@ -4,7 +4,7 @@ venv=$1
 if [ -n "$venv" ]
 then
     source ./${venv}/bin/activate
-    pip install pygdal==$(gdal-config --version).* -r requirements.txt
+    pip install pygdal==$(gdal-config --version).* -r requirements.txt --upgrade
 else
    echo 'Please specify virtualenv'
 fi
