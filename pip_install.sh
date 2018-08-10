de079abb190713ba37627960dcabb18b0251635e
Sebastian Lamprecht <lamprecht@uni-trier.de>
2018-08-10 14:07:57 +0200

    installation script revised

--- pip_install.sh
+++ pip_install.sh
@@ -3,7 +3,9 @@
 VENV=""
 REQUIREMENTS='requirements.txt'
 DEV_REQUIREMENTS='dev_requirements.txt'
-USAGE='Usage:\ninstall.sh -v venv (-d)\n-v [arg] : path of virtualenv'
+USAGE='\nUsage:\n\npip_install.sh -v venv (-d)\n\n\t-v [arg]\t: Specify path to virtual 
+environment.\n\t-d\t\t: Also install packages used for development.\n\t-h\t\t: 
+Display this help.'
 
 requirements="-r $REQUIREMENTS"
 while getopts v:dh option
@@ -27,5 +29,7 @@ then
     source ./${VENV}/bin/activate
     pip install pygdal==$(gdal-config --version).* $requirements --upgrade
 else
-   echo 'Please specify virtualenv'
+    echo "Please specify virtualenv with '-v [arg]'".
+    echo -e $USAGE
+    exit 0
 fi
