47e33447dff5abeba4811b20d246c34334489c58
Sebastian Lamprecht <lamprecht@uni-trier.de>
2018-08-13 12:42:32 +0200

    license updated

--- tests/test_pointspy.py
+++ tests/test_pointspy.py
@@ -1,9 +1,19 @@
 # BEGIN OF LICENSE NOTE
 # This file is part of Pyoints.
-# Copyright (c) 2018, Sebastian Lamprecht, lamprecht@uni-trier.de
+# Copyright (c) 2018, Sebastian Lamprecht, Trier University, 
+# lamprecht@uni-trier.de
 # 
-# This software is copyright protected. A decision on a less restrictive licencing 
-# model will be made before releasing this software.
+# Pyoints is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Pyoints is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
 # END OF LICENSE NOTE
 import unittest
 import doctest
