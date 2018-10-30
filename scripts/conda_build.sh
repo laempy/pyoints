a32c583d215843751b73d5773c3f10eece859d6f
Lamprecht <lamprechtsebastian@gmx.de>
2018-10-30 09:39:26 +0100

    experiments with conda

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(realpath -s $0))
-PYTHON_VERSION="3.5"
 
 # build conda package
-conda build --python ${PYTHON_VERSION} ${SCRIPT_PATH}/../conda
+conda build ${SCRIPT_PATH}/../conda
---
+++ scripts/conda_install.sh
@@ -0,0 +1,2 @@
+conda env remove --name pyoints_env
+conda create --name pyoints_env pyoints --use-local
