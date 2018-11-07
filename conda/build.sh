edab5761bbf3e8790f4da2ad2afb3a1e77e4e1f3
Lamprecht <lamprechtsebastian@gmx.de>
2018-11-07 09:46:23 +0100

    cross platform tests

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,5 @@
-$PYTHON -m pip install . --ignore-installed -vvv
+#$PYTHON -m pip install . --ignore-installed -vvv
+python -m pip install . --ignore-installed -vvv
 # windows
 #python setup.py install --single-version-externally-managed --record=record.txt
 #if errorlevel 1 exit 1
