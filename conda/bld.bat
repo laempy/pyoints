8da01b8e40ed5fd74d613bdff1c1e92085dae5bf
Lamprecht <lamprechtsebastian@gmx.de>
2018-11-08 16:34:31 +0100

    experiments conda

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,3 +1,2 @@
-"%PYTHON%" -m pip install cylinder-fitting plyfile laspy -vvv
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv
+"%PYTHON%" -m pip install . -vvv
 if errorlevel 1 exit 1
--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,11 @@
 #$PYTHON -m pip install cylinder-fitting plyfile laspy -vvv
 #$PYTHON -m pip install . --no-deps --ignore-installed -vvv
-python -m pip install cylinder-fitting plyfile laspy -vvv
-python -m pip install . --no-deps --ignore-installed -vvv
+$PYTHON -m pip install . -vvv
+#python -m pip install . -vvv
+#python -m pip install cylinder-fitting plyfile laspy -vvv
+#python -m pip install . --no-deps --ignore-installed -vvv
+#python -m pip install numpy>=1.15 rtree laspy scipy scikit-learn pyproj dill pandas
+#python -m pip install . -vvv
 # windows
 #python setup.py install --single-version-externally-managed --record=record.txt
 #if errorlevel 1 exit 1
