b6fe216b0283217e2c3242235791852880e15625
Lamprecht <lamprechtsebastian@gmx.de>
2019-02-05 13:56:28 +0100

    conda script revised

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -6,5 +6,11 @@ cd ${SCRIPT_PATH}/../conda
 conda build .
 
 
+# test environment
+#conda env remove -y -n pyoints_test
+#conda create -y -n pyoints_test -c $(conda build . --output) pyoints
+#conda activate pyoints_test
+
+# upload new package
 #anaconda login
 #anaconda upload $(conda build . --output)
