39d11abdb1edbd600f152f1352900c5e14278ea6
Sebastian Lamprecht <lamprecht@uni-trier.de>
2019-03-04 14:04:33 +0100

    docs updated

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'version 0.2.0rc1',
+    VERSION: 'version 0.2.0rc2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
